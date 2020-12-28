ff76a0eb08f0d082fbe930f3355097b4ee1f90ff
manojmpg114 <manojmpg114@gmail.com>
2020-12-27 19:35:40 -0500

    String formating
    f strings and .format method use

--- Python Obect and data stucture basics/StringPropMethod.py
+++ Python Obect and data stucture basics/StringPropMethod.py
@@ -27,3 +27,53 @@ x =  'Hello world'
 # comment x = x.upper()
 print(x.upper())
 
+#without the () python reads x.upper as a request to what it is aka its a function called upper 
+
+#x.split would split the string into a list 
+print(x.split())
+
+x = 'Hi this is a string to split'
+
+print(x.split()) #we can also split on other characters other than white space, it would remove that character in the result 
+
+#x.split(i) would include the white spaces but not the 'i's in the list
+print(x.split('i'))
+
+#String interpolation is putting a variable into a string 
+
+#STRING FORMATTING with .format() method or f-strings (formated string literals)
+
+#formating with .format() method
+
+print('This is a string {}'.format('INSERTED'))
+
+print('The {} {} {}'.format('fox', 'brown', 'quick'))
+
+print('The {2} {1} {0}'.format('fox', 'brown', 'quick'))
+
+#we can also assign keywords
+print('The {q} {b} {f}'.format(f='fox', b ='brown', q ='quick'))
+
+print('The {f} {f} {f}'.format(f='fox', b ='brown', q ='quick'))
+
+#float formatting follows "{value:width.precision f}"
+
+result = 100/777
+print(result)
+
+print("The result was {r}".format(r=result))
+
+
+print("The result was {r:1.3f}".format(r=result))
+
+
+name = 'Manoj'
+print(f'Hello, his name is {name}') #the f strings is the new way to combine variables with string literals more like java and other programs where its easier
+#combine the two
+
+name = 'David'
+age = 26
+
+print(f'{name} is {age}') #always remember its f then string 
+
+
