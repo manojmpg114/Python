ef82bfdb6c4990f8ffbfba7ab8b90503e7202d23
manojmpg114 <manojmpg114@gmail.com>
2020-12-30 00:43:25 -0500

    boolean logical operators
    boolean logical operators 'and' 'or' 'not'

--- Python Obect and data stucture basics/boolean.py
+++ Python Obect and data stucture basics/boolean.py
@@ -13,3 +13,11 @@ print(1<2)
 print(1>2)
 
 b = None #None is captital N and this is used as a place holder so we don't get a message saying that this is not defined
+
+#in python logical comparators are written out
+# && = "and" in python
+#you can also use parenthesis to separate the conditions 
+# || in java is just "or" in python
+#should always focus on readability in the code
+
+#"not" keyword provides the opposite impact like not (1==1) returns a False 
