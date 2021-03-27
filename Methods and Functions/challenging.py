8a7e9d3a5a9c9972b9bed5e5509b4f9d9d6c5f53
manojmpg114 <manojmpg114@aol.com>
2021-03-27 12:22:58 -0400

    Update challenging.py

--- Methods and Functions/challenging.py
+++ Methods and Functions/challenging.py
@@ -39,3 +39,32 @@ def spy_game2(nums):
 print(spy_game([7,0,7,0,7,0]))
 
 print(spy_game2([7,0,7,0,7,0]))
+
+def count_primes(num): #very math orientated 
+    #check for 0 or 1 input
+
+    if num < 2:
+        return 0
+    # 2 or greater
+    primes = [2]
+    
+    #counter going up to the input num
+    x = 3
+
+    #x is going through every number up to input num
+    while x <= num:
+        #check if x is prime
+        #for y in range (3,x,2):
+        for y in primes:
+            if x%y == 0:
+                x += 2 # we skip by 2's so we can avoid all even numbers
+                #prime numbers are not even
+                break
+        else: # FOR ELSE STATEMENT -- unique to python
+            primes.append(x)
+            x += 2
+    print(primes)
+    len(primes)
+    print(len(primes))
+
+count_primes(100)
